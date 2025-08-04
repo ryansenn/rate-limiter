a59ef625cdf6f3dacbbed66e6af5d7d0e3a253ee
ryan <ryansen87@gmail.com>
2025-08-04 14:49:30 -0700

    raw tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+use tokio::net::TcpListener;
+use tokio::spawn;
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() {
+    server().await;
+}
+
+
+async fn server(){
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        spawn(handle_connection(socket));
+    }
+
+}
+
+async fn handle_connection(mut socket: TcpStream){
+    let mut buf = [0u8; 1024];
+    let size = socket.read(&mut buf).await.unwrap();
+
+    handle_request(&buf[..size]).await;
+}
+
+async fn handle_request(buf: &[u8]){
+    println!("new request");
 }
